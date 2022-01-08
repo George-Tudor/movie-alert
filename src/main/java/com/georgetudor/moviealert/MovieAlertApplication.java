9a92587d75d46ae4d4aa4ee97c4911f040cefe62
George Tudor <Alexander.George.Tudor@gmail.com>
2022-01-07 22:47:01 -0600

    created basic html files

---
+++ src/main/java/com/georgetudor/moviealert/MovieAlertApplication.java
@@ -0,0 +1,13 @@
+package com.georgetudor.moviealert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieAlertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieAlertApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/georgetudor/moviealert/MovieAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgetudor.moviealert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieAlertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
