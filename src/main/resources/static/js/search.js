329fe453cbf8382c948c88f04b2e0ab44ef13254
George Tudor <Alexander.George.Tudor@gmail.com>
2022-01-09 23:07:35 -0600

    update

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -1,7 +1,13 @@
 $(() => {
 
-    $("body").on('click', '#movieSearch', function(){
+    $("body").on('click', '#movieSearch', function () {
         console.log('testing')
-    })
+            fetch('https://api.themoviedb.org/3/search/movie?api_key=' + key + '&language=en-US&query=fight&page=1&include_adult=false')
+                .then(response => {
+                    return console.log(response.json());
+                })
 
-})
+                })
+        });
+
+
